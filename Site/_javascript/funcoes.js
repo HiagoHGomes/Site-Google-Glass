fe7a34190b7a715a5ba58a3b9bca728298aed09d
Hiago Henrique Gomes <hiago_h3@hotmail.com>
2021-10-03 12:40:23 -0300

    alteração logo

--- Site/_javascript/funcoes.js
+++ Site/_javascript/funcoes.js
@@ -5,3 +5,15 @@ function mudaFoto (foto) {
 function mudaSpecs (foto) {
     document.getElementById ("specs").src = foto;
 }
+
+function mudaPhoto (foto) {
+    document.getElementById ("photo").src = foto;
+}
+
+function mudaMult (foto) {
+    document.getElementById ("mult").src = foto;
+}
+
+function mudaTel (foto) {
+    document.getElementById ("telefone").src = foto;
+}
